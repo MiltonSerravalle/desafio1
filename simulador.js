dcf09b4fd3c0781e51616e7cd573a690b66d037f
Milton Serravalle <milton.serravalle@hotmail.com>
2022-09-12 19:03:56 -0300

    Error camelCase

--- simulador.js
+++ simulador.js
@@ -8,9 +8,9 @@ const notas = (a, b, c) => {
 
 while(escribeNumero ==="si" || escribeNumero ==="Si" || escribeNumero === "SI"){
     alert("A continuacion ingrese las notas de los 3 trimestres");
-    let notaA = parseint(prompt("Ingrese la primer nota del trimestre"));
-    let notaB = parseint(prompt("Ingrese la segunda nota del trimestre"));
-    let notaC = parseint(prompt("ingrese la tercer nota del trimestre"));
+    let notaA = parseInt(prompt("Ingrese la primer nota del trimestre"));
+    let notaB = parseInt(prompt("Ingrese la segunda nota del trimestre"));
+    let notaC = parseInt(prompt("ingrese la tercer nota del trimestre"));
     let promedio = notas(notaA, notaB, notaC);
     alert(`el promedio de las notas ingresadas es igual a ${promedio}`);
     escribeNumero = prompt("ingrese si para cargar notas");
